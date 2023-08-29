6139be2d318408e11651b8aff745aa2c47de9a60
mamemaru00 <k.shouhei0711@gmail.com>
2023-08-29 22:18:35 +0900

    react-cookie01

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// useCookiesをimport
+import { useCookies } from "react-cookie";
 
-function App() {
+const App = () => {
+  // usernameの状態を管理するためのuseStateを定義
+  const [UserNameVal, setUserNameVal] = useState("");
+  // ageの状態を管理するためのuseStateを定義
+  const [AgeVal, setAgeVal] = useState("");
+  // cookie名として、usernameとageを設定する
+  const [cookies, setCookie, removeCookie] = useCookies(["username", "age"]);
+
+  const userNameChange = (e) => {
+    setUserNameVal(e.target.value);
+  };
+
+  const AgeChange = (e) => {
+    setAgeVal(e.target.value);
+  };
+
+  const CookieDataDelete = () => {
+    removeCookie("username");
+    removeCookie("age");
+  };
+
+  const CookieDataAdd = () => {
+    setCookie("username", UserNameVal);
+    setCookie("age", AgeVal);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="wrap">
+      <div className="main">
+        <h2>ユーザー登録</h2>
+        {/* // 氏名が変更されたらuserNameChange関数を呼び出す */}
+        <input type="text" name="useradd" onChange={(e) => userNameChange(e)} className="input_margin" />
+        {/* // 年齢が変更されたらAgeChange関数を呼び出す */}
+        <input type="text" name="useradd" onChange={(e) => AgeChange(e)} className="input_margin" />
+        <div className="button-group">
+          {/* // 保存ボタンを押下したら、CookieDataAdd関数を呼び出す */}
+          <button onClick={() => CookieDataAdd()}>保存</button>
+          {/* // 削除ボタンを押下したら、CookieDataDelete関数を呼び出す */}
+          <button onClick={() => CookieDataDelete()}>削除</button>
+        </div>
+        <br />
+        <hr />
+        <h3>値を参照(取得)</h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          氏名<br />
+          {cookies.username ? cookies.username + "さん" : ""}</p>
+        <p>
+          年齢<br />
+          {cookies.age ? cookies.age + "歳" : ""}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
