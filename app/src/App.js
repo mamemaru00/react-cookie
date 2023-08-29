e761f5e0d04c6e63e19797fbcd3b96431f574e61
mamemaru00 <k.shouhei0711@gmail.com>
2023-08-29 22:47:28 +0900

    react-cookie02

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import { useCookies } from "react-cookie";
 
 function App() {
+
+  const [name, setName] = useState();
+  const [cookies, setCookie, removeCookie] = useCookies();
+
+  const handlerChange = (e) => {
+    setName(e.target.value);
+  };
+  const handlerDelete = () => {
+    removeCookie("name");
+  };
+  const handleSubmit = () => {
+    setCookie("name", name);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input type="text" name="name" onChange={handlerChange} />
+        </label>
+        <input type="submit" value="保存" />
+        <button onClick={handlerDelete}>削除</button>
+        {cookies.name}
+      </form>
     </div>
   );
 }
